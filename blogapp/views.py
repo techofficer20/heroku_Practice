b62318ccc829727403c27fea4859a070d5f8f4b2
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-20 22:43:46 +0900

    detail page recommit

--- blogapp/views.py
+++ blogapp/views.py
@@ -8,4 +8,4 @@ def home(request):
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': blog_detail})
+    return render(request, 'detail.html', {'blog': blog_detail})
