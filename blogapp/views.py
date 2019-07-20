50a195a0a8b78e69f617d8a6c44f6ee9d474c8a6
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-20 22:23:28 +0900

    blog test commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs' : blogs})
