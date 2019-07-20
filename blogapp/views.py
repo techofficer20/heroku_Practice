c8600cd390cf4a696aa1ee1c8c0d878bd94093c3
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-20 22:53:01 +0900

    write page add

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
+from django.utils import timezone
 # Create your views here.
 
 def home(request):
@@ -9,3 +10,14 @@ def home(request):
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'blog': blog_detail})
+
+def new(request):
+    return render(request, 'blog/new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
