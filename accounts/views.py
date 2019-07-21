e08b326bddfe24da09262c831d64c6534ed21a03
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-21 22:10:15 +0900

    accounts app add recommit

--- accounts/views.py
+++ accounts/views.py
@@ -27,5 +27,5 @@ def login(request):
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
-        return redirect('blog')
+        return redirect('home')
     return render(request, 'signup.html')
