a4ed4948c980f3c997f6d3ecafda72f3df9dd28a
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-26 20:00:13 +0900

    before finish portfolio image upload

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/images/'
+MEDIA_URL = '/media/'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
